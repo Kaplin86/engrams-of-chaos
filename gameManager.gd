4772e59d86017b8a6abd62deb28a951e409ce5af
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2025-10-17 13:23:28 -0400

    55 hours and 28 minutes
    added double pattern and changed background on leaderboard

--- gameManager.gd
+++ gameManager.gd
@@ -46,8 +46,8 @@ func _ready() -> void:
 	$CanvasLayer2/Control.visible = true
 	
 	if DatastoreHolder.difficulty == "TurnedTables":
-		#spawnUnit(currentlyAvailableBosses.pick_random(),Vector2i(7,14),2,true)
-		spawnUnit("chaotic_one",Vector2i(7,14),2,true)
+		spawnUnit(currentlyAvailableBosses.pick_random(),Vector2i(7,14),2,true)
+		#spawnUnit("chaotic_one",Vector2i(7,14),2,true)
 	else:
 		if !DatastoreHolder.tutorial:
 			spawnUnit( DatastoreHolder.synergyUnitJson.keys().pick_random(),Vector2i(7,14),2)
