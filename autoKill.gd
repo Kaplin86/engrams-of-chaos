ae243ef69fe1a2d593d09a16f53379d2c7da167f
Kaplin86 <87865073+Kaplin86@users.noreply.github.com>
2025-11-03 20:48:38 -0500

    95h 17m
    Made some changes to the autokill, but more importantly started work on a thanks to credits page that will show up when you put in konami code. Im doing a commit this early since I gotta swap devices

--- autoKill.gd
+++ autoKill.gd
@@ -1,8 +1,17 @@
 extends Node
 
-## This script will automatically kill the window after no inactivity (for multi-game arcade system).
+## This script will automatically kill the window after no inactivity (for multi-game arcade system). It will also set the locale based on how this is executed
 
 var timeSinceLastInput = 0.0
+func _ready():
+	# Parse command line arguments
+	var args = OS.get_cmdline_args()
+	for arg in args:
+		if arg.begins_with("--locale="):
+			var locale = arg.substr("--locale=".length())
+			TranslationServer.set_locale(locale)
+			print("Locale set to:", locale)
+			
 func _process(delta):
 	if OS.has_feature("web"):
 		return
